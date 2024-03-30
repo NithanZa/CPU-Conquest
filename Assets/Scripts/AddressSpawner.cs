c1638a570b9fa58b522e515c8083ae3efe0f2a23
NithanZa <jaroonekp@gmail.com>
2024-03-30 14:00:53 +0700

    texts and address bus

--- Assets/Scripts/AddressSpawner.cs
+++ Assets/Scripts/AddressSpawner.cs
@@ -14,13 +14,13 @@ public class AddressSpawner : MonoBehaviour
         if (gameObject.CompareTag("InstructionMonster")) {
             GameObject iat = Instantiate(textPrefab, player.transform);
             iat.name = "IAT";
-            iat.transform.localPosition = new Vector3(0f, 1f, -1f);
+            iat.transform.localPosition = new Vector3(0f, 0.3f, -1f);
             string address = Convert.ToString(UnityEngine.Random.Range(0, 7), 2).PadLeft(4, '0');
             iat.GetComponent<TextMeshPro>().SetText(address);
         } else if (gameObject.CompareTag("DataMonster")) {
             GameObject dat = Instantiate(textPrefab, player.transform);
             dat.name = "DAT";
-            dat.transform.localPosition = new Vector3(0f, 0.5f, -1f);
+            dat.transform.localPosition = new Vector3(0f, 0.2f, -1f);
             string address = Convert.ToString(UnityEngine.Random.Range(8, 15), 2).PadLeft(4, '0');
             dat.GetComponent<TextMeshPro>().SetText(address);
         }
