2450ab10ccf2538bf6be1da7d731a353e7571f55
NithanZa <jaroonekp@gmail.com>
2024-03-30 13:03:05 +0700

    Text prefabs yay

--- Assets/Scripts/AddressSpawner.cs
+++ Assets/Scripts/AddressSpawner.cs
@@ -12,17 +12,17 @@ public class AddressSpawner : MonoBehaviour
     
     public void CheckEntityAndSpawnAddress() {
         if (gameObject.CompareTag("InstructionMonster")) {
-            GameObject text = Instantiate(textPrefab, player.transform);
-            text.name = "IAT";
-            text.transform.localPosition = new Vector3(0f, 1f, -1f);
+            GameObject iat = Instantiate(textPrefab, player.transform);
+            iat.name = "IAT";
+            iat.transform.localPosition = new Vector3(0f, 1f, -1f);
             string address = Convert.ToString(UnityEngine.Random.Range(0, 7), 2).PadLeft(4, '0');
-            text.GetComponent<TextMeshPro>().SetText(address);
+            iat.GetComponent<TextMeshPro>().SetText(address);
         } else if (gameObject.CompareTag("DataMonster")) {
-            GameObject text = Instantiate(textPrefab, gameObject.transform.position, Quaternion.identity);
-            text.name = "DAT";
-            text.transform.localPosition = new Vector3(0f, 0.5f, -1f);
+            GameObject dat = Instantiate(textPrefab, player.transform);
+            dat.name = "DAT";
+            dat.transform.localPosition = new Vector3(0f, 0.5f, -1f);
             string address = Convert.ToString(UnityEngine.Random.Range(8, 15), 2).PadLeft(4, '0');
-            text.GetComponent<TextMeshPro>().SetText(address);
+            dat.GetComponent<TextMeshPro>().SetText(address);
         }
     }
 }
